25fc691881583568fdb350c4c1484a3571631556
root <root@DESKTOP-7O5VQU7.localdomain>
2024-05-18 02:28:21 +0300

    new update

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -25,34 +25,34 @@ def hello_hbnb():
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route("/c/<text>", strict_slashes=False)
 def dynamic_route(text):
     """Displays 'C' followed by the value of <text>."""
     text = text.replace("_", " ")
     return f"C {text}"
 
 
-@app.route('/python/')
-@app.route('/python/<text>', strict_slashes=False)
+@app.route("/python/")
+@app.route("/python/<text>", strict_slashes=False)
 def dynamic_route_pyton(text="is cool"):
     """Displays 'C' followed by the value of <text>."""
     text = text.replace("_", " ")
     return f"Python {text}"
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    """ display “n is a number” only if n is an integer"""
+    """display “n is a number” only if n is an integer"""
     return f"{n} is a number"
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     """Display HTML only if n is an integer."""
-    return render_template('5-number.html', n=n)
+    return render_template("5-number.html", n=n)
 
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
     """Display Number: n is even|odd in HTML only if n is an integer."""
 
@@ -60,4 +60,4 @@ def number_odd_or_even(n):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
