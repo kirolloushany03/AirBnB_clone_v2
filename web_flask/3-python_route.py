de70d074a30cce04278dc92b039e0209a2cd4bed
root <root@DESKTOP-7O5VQU7.localdomain>
2024-05-17 22:58:56 +0300

    new update

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" module serves as a flask starter and makeing another page"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home():
+    """return Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """/hbnb route : dispaly HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def dynamic_c_route(text):
+    """/dynamic c route : dispaly HBNB"""
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def dynamic_python_route(text):
+    """/dynamic c route : dispaly HBNB"""
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
